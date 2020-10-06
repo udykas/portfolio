46ad407e0d588e091b368cc43d245f6d6af1d4e3
udykas <udykas@gmail.com>
2020-10-06 17:10:45 -0400

    Started landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="landing-page-img"></div>
+        <div className="landing-page-text">
+          <h1>ALEX DYKAS</h1>
+          <h4>FULLSTACK WEB DEVELOPER <br/> & <br/> SOFTWARE ENGINEER</h4>
+        </div>
       </header>
     </div>
   );
