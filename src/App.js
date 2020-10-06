b25b81d778076bf2350951c30044ac04eec005fe
udykas <udykas@gmail.com>
2020-10-06 18:02:33 -0400

    Updated landing page animation

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <div className="landing-page-img"></div>
         <div className="landing-page-text">
-          <h1>ALEX DYKAS</h1>
-          <h4>FULLSTACK WEB DEVELOPER <br/> & <br/> SOFTWARE ENGINEER</h4>
+          <hr className="line" style={{color: 'white', height: '1px', backgroundColor: 'white'}}/>
+          <h2 className="name">ALEX DYKAS</h2>
+          <hr className="line" style={{color: 'white', height: '1px', backgroundColor: 'white'}}/>
+          <p className="occupation">FULLSTACK WEB DEVELOPER <br/> & <br/> SOFTWARE ENGINEER</p>
         </div>
       </header>
     </div>
