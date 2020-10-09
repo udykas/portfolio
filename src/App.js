3e4a01e3cb8b510f6a4dcce9ab4b0120e7a19c7b
udykas <udykas@gmail.com>
2020-10-09 15:06:40 -0400

    Added animation to open and close menu

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
   const handleMenu = () => {
     const menu = document.getElementsByClassName("menu");
     if(menu[0].classList.contains("active")){
+      menu[0].classList.add("inactive")
       menu[0].classList.remove("active")
     } else {
       menu[0].classList.add("active")
+      menu[0].classList.remove("inactive")
     }
     console.log(menu[0].classList)
   }
